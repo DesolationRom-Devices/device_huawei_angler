2282b618a519b83169e5586b5a3a1276d019198b
Noah Greene <chevycam94@gmail.com>
2016-02-01 01:57:32 -0500

    Revert "Migrated dumpstate to C++"
    This reverts commit 603524c79489668a23b523a3a6ced325fb6ea9ef.

--- dumpstate/Android.mk
+++ dumpstate/Android.mk
@@ -17,7 +17,7 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := frameworks/native/cmds/dumpstate
 
-LOCAL_SRC_FILES := dumpstate.cpp
+LOCAL_SRC_FILES := dumpstate.c
 
 LOCAL_MODULE := libdumpstate.angler
 
