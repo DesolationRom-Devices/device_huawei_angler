79df153f5be75a7d1b84edca272fe0f20986ac54
Noah Greene <chevycam94@gmail.com>
2016-01-31 22:33:52 -0500

    Use Cortex CPU optimizations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,12 +22,12 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a7
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
 # Kernel Toolchain
 TARGET_GCC_VERSION_ARM64 := 6.0-kernel-uber
@@ -35,7 +35,7 @@ TARGET_GCC_VERSION_ARM64 := 6.0-kernel-uber
 # Optimizations
 STRICT_ALIASING := false
 CLANG_O3 := true
-ENABLE_GCCONLY := true
+ENABLE_GCCONLY := false
 GRAPHITE_OPTS := false
 USE_PIPE := true
 
