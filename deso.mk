e03376f769178c2b9f25e82095162d8627ebfb40
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-10-26 13:38:05 -0500

    Add deso.mk

---
+++ deso.mk
@@ -0,0 +1,19 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/deso/config/common_full_phone.mk)
+$(call inherit-product, vendor/deso/config/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := angler
+PRODUCT_NAME := deso_angler
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := Angler
+PRODUCT_MANUFACTURER := huawei
+
+# TARGET_GCC_VERSION_ARM := 5.4-gnu
+# TARGET_GCC_VERSION_EXP := 4.9
